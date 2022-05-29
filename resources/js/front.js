0388beca0f6997f0b1e05e49fe36e5c2567eb53d
cif95 <vivianacifarelli9@gmail.com>
2022-05-29 17:19:57 +0200

    implemented router.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,50 +1,14 @@
-window.axios = require('axios');
+require('./bootstrap');
+
+import Axios from 'axios';
+import App from "./views/App.vue";
+import router from './router.js';
 
 window.Vue = require('vue');
-
-// import router from './router';
-
-
-import App from "./views/App.vue";
-import VueRouter from 'vue-router';
-import Home from './pages/Home';
-import Posts from './pages/Posts';
-import PostDetail from './pages/PostDetail';
-import AboutUs from './pages/AboutUs';
-
-window.Vue.use(VueRouter);
-
-
-const router = new VueRouter({
-	mode: 'history',
-	routes : [
-			{
-					path: '/',
-					name: 'home',
-					component: Home
-			},
-			{
-					path: '/posts',
-					name: 'posts',
-					component: Posts
-			},
-			{
-					path: '/about-us',
-					name: 'about-us',
-					component: AboutUs
-			},
-			{
-				path: '/posts/:id',
-				name: 'post_detail',
-				component: PostDetail
-			}
-
-	]
-});
 
 
 const app = new Vue({
     el: '#root',
+		router: router,
     render: h => h(App),
-    router
 });
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,32 +1,40 @@
-// import VueRouter from 'vue-router';
-// import Home from './pages/Home';
-// import Posts from './pages/Posts';
-// import AboutUs from './pages/AboutUs';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-// window.Vue.use(VueRouter);
+// components
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+import PostDetail from './pages/PostDetail';
+import AboutUs from './pages/AboutUs';
 
+Vue.use(VueRouter);
 
-// const router = new VueRouter({
-// 	mode: 'history',
-// 	routes : [
-// 			{
-// 					path: '/home',
-// 					name: 'home',
-// 					component: Home
-// 			},
-// 			{
-// 					path: '/posts',
-// 					name: 'posts',
-// 					component: Posts
-// 			},
-// 			{
-// 					path: '/about-us',
-// 					name: 'about-us',
-// 					component: AboutUs
-// 			},
-// 	]
-// });
+// routes
+const router = new VueRouter({
+	mode: 'history',
+	routes : [
+			{
+					path: '/',
+					name: 'home',
+					component: Home
+			},
+			{
+					path: '/posts',
+					name: 'posts',
+					component: Posts
+			},
+			{
+					path: '/about-us',
+					name: 'about-us',
+					component: AboutUs
+			},
+			{
+				path: '/posts/:id',
+				name: 'post_detail',
+				component: PostDetail
+			}
+	]
+});
 
-// export default router;
-
+export default router;
 
