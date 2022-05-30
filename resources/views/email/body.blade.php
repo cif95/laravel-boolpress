7fbf0b11b139f002a3dd6fb520044ac3365373f1
cif95 <vivianacifarelli9@gmail.com>
2022-05-30 17:16:37 +0200

    styled email form

--- resources/views/email/body.blade.php
+++ resources/views/email/body.blade.php
@@ -4,11 +4,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 	<title>Mail from Boolpress</title>
 </head>
 <body>
-	<h5>Message from {{$author}} via Boolpress: </h5>
-	<h4>{{$title}}</h4>
-	<p>{{$content}}</p>
+	
+	<section id="email-body" class="container-fluid">
+		<div class="row">
+			<div class="col-12">
+				<p class="fst-italic">Message from {{$author}} via Boolpress: </p>
+				<h4>{{$title}}</h4>
+				<p>{{$content}}</p>
+			</div>
+		</div>
+	</section>
+
 </body>
 </html>
--- resources/views/guest/contact.blade.php
+++ resources/views/guest/contact.blade.php
@@ -4,22 +4,31 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 	<title>Contact</title>
 </head>
 <body>
 	<main>
-		<section class="container-fluid">
+		<section class="container-fluid p-5">
 			<div class="row">
-				<div class="col-5 mx-auto">
-					<h2>Send a message:</h2>
-					<form action="{{ route('guest.storeContact')}}" method="post">
+				<div class="col-5 mx-auto p-5">
+					<h2>Send us a message:</h2>
+					<form action="{{ route('guest.storeContact')}}" method="post" class="d-flex flex-column py-3">
 						@csrf
 						@method('POST')
-						<input type="text" placeholder="type your email address.." name="guest_email">
-						<input type="text" placeholder="type your full name.." name="guest_name">
-						<input type="text" placeholder="type your message title.." name="guest_emailTitle">
-						<textarea type="text" placeholder="type your message.." name="guest_message"></textarea>
-						<button type="submit">Send message</button>
+							<label for="guest_email">Email address:</label>
+							<input type="text" placeholder="type your email address.." name="guest_email" class="form-control mb-4">
+
+							<label for="guest_name">Name:</label>
+							<input type="text" placeholder="type your full name.." name="guest_name" class="form-control mb-4">
+
+							<label for="guest_emailTitle">Title:</label>
+							<input type="text" placeholder="type your message title.." name="guest_emailTitle" class="form-control mb-4">
+
+							<label for="guest_message">Message:</label>
+							<textarea type="text" placeholder="type your message.." name="guest_message" class="form-control mb-4"></textarea>
+
+							<button class="btn btn-primary w-25 mx-auto" type="submit">Send message</button>
 					</form>
 				</div>
 			</div>
